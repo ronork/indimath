06c5fb3acd7ba5757b9278e36ae55e9c43e8cb52
Rohan <grohan95@gmail.com>
2016-07-21 20:55:10 +0530

    final commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,19 @@
 class PostsController < ApplicationController
 	before_action :find_post, only: [:show, :edit, :update, :destroy]
 	before_action :authenticate_user!, except: [:index, :show]
+
 	def index
 		@posts = Post.all.order("created_at DESC")
+	end
+
+	def show
 	end
 
 	def new
 		@post = current_user.posts.build
 	end
 
-	def create 
+	def create
 		@post = current_user.posts.build(post_params)
 
 		if @post.save
@@ -18,24 +22,22 @@ class PostsController < ApplicationController
 			render 'new'
 		end
 	end
-	def show
-	end
 
 	def edit
 	end
 
-    def update
-    	if @post.update(post_params)
-    		redirect_to @post
-    	else
-    		render 'edit'
-    	end
-    end
+	def update
+		if @post.update(post_params)
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
 
-    def destroy
-    	@post.destroy
-    	redirect_to root_path
-    end
+	def destroy
+		@post.destroy
+		redirect_to root_path
+	end
 
 	private
 
